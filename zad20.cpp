c935f0f2daf7b1cf93e1f6d3101ff688fd4fc0f2
Zakozny <zkazony.909@gmail.com>
2026-06-02 13:21:39 +0200

    Add files via upload

---
+++ zad20.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float bok, pole;
+    char odp;
+
+    do
+    {
+        cout << "Podaj bok kwadratu w centymetrach: ";
+        cin >> bok;
+
+        if (bok > 0)
+        {
+            pole = bok * bok;
+            cout << "Pole kwadratu = " << pole << " cm^2" << endl;
+        }
+        else
+        {
+            cout << "Bok musi byc wiekszy od zera!" << endl;
+        }
+
+        cout << "Jesli chcesz powtorzyc program, nacisnij t (tak): ";
+        cin >> odp;
+
+    } while (odp == 't' || odp == 'T');
+
+    return 0;
+}
