680c51a1657582aa066bce442ddd94fefe0213fc
Zakozny <zkazony.909@gmail.com>
2026-06-17 16:24:20 +0200

    Refactor zad19.cpp for clarity and correctness

--- zad19.cpp
+++ zad19.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <streambuf>
-#include <ios>
-#include <iosfwd>
 using namespace std;
-	
-int main(int argc, char** argv) {
-	 system ("cls");
- system ("chcp 1250");
 
- int liczba, wynik;
-
-	cout<< "podaj liczbe ca³kolwit¹";
-	cin>> liczba ;
-	
-	if(liczba > 0){
-	cout<<"liczba wieksza o";
-}
-	if(liczba = 0){
-		cout<<"liczba rowna zero";
-	}
-	if(liczba < 0){
-		cout<<"liczba mniejsza od zera";
-	}
-	return 0;
+int main() {
+    system("cls");
+    
+    int liczba;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> liczba;
+    
+    if (liczba > 0) {
+        cout << "liczba wieksza zera" << endl;
+    }
+    if (liczba == 0) { 
+        cout << "liczba rowna zero" << endl;
+    }
+    if (liczba < 0) {
+        cout << "liczba mniejsza od zera" << endl;
+    }
+    
+    return 0;
 }
